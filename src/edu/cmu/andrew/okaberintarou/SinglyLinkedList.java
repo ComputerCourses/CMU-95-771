f91bfec826da625524912a2e509295b5597dc7e5
OkabeRintarou <2573269786@qq.com>
2018-08-06 10:17:43 +0800

    Homework1 Part1

---
+++ src/edu/cmu/andrew/okaberintarou/SinglyLinkedList.java
@@ -0,0 +1,75 @@
+package edu.cmu.andrew.okaberintarou;
+
+import edu.colorado.nodes.ObjectNode;
+
+import java.math.BigInteger;
+
+public class SinglyLinkedList {
+
+    private ObjectNode head;
+
+    private ObjectNode tail;
+
+    private int countNodes;
+
+    private ObjectNode iter;
+
+    public SinglyLinkedList() {
+        head = tail = null;
+        iter = null;
+        countNodes = 0;
+    }
+
+    public void reset() {
+        iter = head;
+    }
+
+    public Object next() {
+        Object r = iter.getData();
+        iter = iter.getLink();
+        return r;
+    }
+
+    public boolean hasNext() {
+        return iter != null;
+    }
+
+    public void addAtEndNode(Object c) {
+        if (tail == null) {
+            head = tail = new ObjectNode(c, null);
+        } else {
+            tail.addNodeAfter(c);
+            tail = tail.getLink();
+        }
+    }
+
+    public int countNodes() {
+        return countNodes;
+    }
+
+    public Object getObjectAt(int i) {
+        return ObjectNode.listPosition(head, i).getData();
+    }
+
+    public Object getLast() {
+        return tail.getData();
+    }
+
+    public String toString() {
+        if (head == null) {
+            return "";
+        }
+        return head.toString();
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        for (long i = 0; i < 26; i++) {
+            list.addAtEndNode(new BigInteger("" + i));
+        }
+        list.reset();
+        while (list.hasNext()) {
+            System.out.println(list.next());
+        }
+    }
+}
